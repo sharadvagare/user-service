89d44ea529455fbac5f6b5892ad51d79671899b5
sharadvagare <42869298+sharadvagare@users.noreply.github.com>
2021-11-25 12:09:47 +0530

    Update UserServiceApplication.java

--- src/main/java/com/dailycodebuffer/user/UserServiceApplication.java
+++ src/main/java/com/dailycodebuffer/user/UserServiceApplication.java
@@ -12,6 +12,8 @@ public class UserServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 		System.out.println("Started Application");
+		System.out.println("Started Application");
+		
 	}
 
 	@Bean
